12e2bfc4189a2eaaf5eb6a8c760f0afcf570fd55
wrolanda <Nikita.Mitriacko.v@gmail.com>
2022-03-26 21:58:42 +0300

    yup, shema and errorsText

--- src/Components/StepOne.js
+++ src/Components/StepOne.js
@@ -1,33 +1,57 @@
 import React from "react";
-import {MainContainer} from "./MainContainer";
 import Typography from "@material-ui/core/Typography";
 import {Form} from "./Form";
 import {Input} from "./Input";
 import {useForm} from "react-hook-form";
+import {MainContainer} from "./MainContainer";
 import {PrimaryButton} from "./PrimaryButton";
+import * as yup from "yup";
+import {yupResolver} from "@hookform/resolvers/yup";
+
+const schema = yup.object().shape({
+    firstName: yup
+        .string()
+        .matches(/^([^0-9]*)$/, "First name should not contain numbers")
+        .required("First name is a required field"),
+    lastName: yup
+        .string()
+        .matches(/^([^0-9]*)$/, "Last name should not contain numbers")
+        .required("Last name is a required field")
+});
 
 export const StepOne = () => {
-    const {register, handleSubmit, errors} = useForm({
-        mode: "onBlur"
-    })
+    const {register, handleSubmit, formState: {errors}} = useForm({
+        mode: "onBlur",
+        resolver: yupResolver(schema),
+    });
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
 
     return (
         <MainContainer>
             <Typography component="h2" variant="h5">
                 Step 1
             </Typography>
-            <Form>
+            <Form onSubmit={handleSubmit(onSubmit)}>
                 <Input
                     {...register('firstName')}
+                    id="firstName"
                     type="text"
                     label="First Name"
-                    name="firsName"
+                    name="firstName"
+                    error={!!errors.firstName}
+                    helperText={errors?.firstName?.message}
                 />
                 <Input
                     {...register('lastName')}
+                    id="lastName"
                     type="text"
                     label="Last Name"
                     name="lastName"
+                    error={!!errors.lastName}
+                    helperText={errors?.lastName?.message}
                 />
                 <PrimaryButton>Next</PrimaryButton>
             </Form>
